d147842ac1449473471b79c537ec48ed27cce90f
Kevin B. Ridgway <kridgway@gmail.com>
2021-09-08 14:20:07 -0400

    Printing just filename.

--- src/play_loop.rs
+++ src/play_loop.rs
@@ -1,11 +1,19 @@
 use color_eyre::Result;
+use std::path::Path;
 
 pub fn play_file(url: &str) -> Result<()> {
-    println!("Playing {:?} on loop", url);
+    print_playing_filename(&url);
 
     for _i in 0..=10 {
         play::play(&url)?;
     }
 
     Ok(())
+}
+
+fn print_playing_filename(url: &str) -> Option<()> {
+    let path = Path::new(url);
+    let filename = path.file_name()?.to_str()?;
+    println!("Playing {} on loop", filename);
+    None
 }
