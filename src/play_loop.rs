e8b9ab1b251386363758fe07a6f2384ab74991b2
Kevin B. Ridgway <kridgway@gmail.com>
2021-09-08 11:01:40 -0400

    Update to request for help.

--- src/play_loop.rs
+++ src/play_loop.rs
@@ -1,26 +1,49 @@
-use std::{path::{Path, PathBuf}, string};
+use std::path::Path;
 
 use color_eyre::Result;
 
-pub fn play_file(looper_path: &PathBuf, _url: Option<String>) -> Result<()> {
-	// let filepath = "./Users/kevin/Music/iTunes/iTunes Media/Music/Chevelle/The North Corridor/1-03 Joyride (Omen).mp3";
-	let filepath2 = "/Users/kevin/Music/Downloaded by MediaHuman/Fabrizio Paterlini - Rue des trois frères.mp3";
-	let filename = Path::new(filepath2).file_name();
-	let printable_filename: String = filename.unwrap().to_str().unwrap().into();
-	println!("Playing {} on loop", printable_filename);
-	let mut n = 1;
-	while n <= 10  {
-		play::play(filepath2).unwrap();
-		// play::play("./Users/kevin/Code/looper/tests/fixtures/sound.mp3").unwrap();
-		n+=1;
-	}
-	Ok(())
+pub fn play_file(url: Option<String>) -> Result<()> {
+    let printable_filename = match url {
+        Some(url) => {
+            let user_provided_filepath = url.clone();
+            let filepath = user_provided_filepath;
+            let filename = Path::new(&filepath).file_name();
+            filename.unwrap().to_str();
+        }
+        None => {
+            println!("Sorry you need to provide a url to play.");
+        }
+    };
+
+    println!("Playing {:?} on loop", printable_filename);
+
+    let mut n = 1;
+    while n <= 10 {
+        // TODO: figure out how to use this proper and passing the path to the play method when it was a Option<String>
+        /*
+         * When running this: cargo run play --url /Users/kevin/Code/looper/tests/fixtures/sound.mp3
+         * compiling looper v0.1.0 (/Users/kevin/Code/looper)
+         * error[E0620]: cast to unsized type: `Option<String>` as `dyn AsRef<Path>`
+         *   --> src/play_loop.rs:24:20
+         *      |
+         *      24 |         play::play(url as dyn AsRef<Path>)?;
+         *         |                    ^^^^^^^^^^^^^^^^^^^^^^
+         *            |
+         *            help: consider using a box or reference as appropriate
+         *              --> src/play_loop.rs:24:20
+         *                 |
+         *                 24 |         play::play(url as dyn AsRef<Path>)?;
+         *                    |                    ^^^
+         *
+         *                    error: aborting due to previous error
+         *
+         *                    For more information about this error, try `rustc --explain E0620`.
+         *                    error: could not compile `looper`))
+         *
+         */
+        play::play(url as dyn AsRef<Path>)?;
+        n += 1;
+    }
+
+    Ok(())
 }
-
-/*
-    let os_str = OsStr::new("example.txt");
-    let path = Path::new(os_str);
-    let extensioner = path.extension();
-    let my_new_string: String = extensioner.unwrap().to_str().unwrap().into();
-
-*/
